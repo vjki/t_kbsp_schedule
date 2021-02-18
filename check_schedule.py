608a16b0fd92c9bd5b1c05c43a207e3223f05552
klimentlamonov <1_0_91@bk.ru>
2021-02-18 18:10:39 +0300

    little changes

--- check_schedule.py
+++ check_schedule.py
@@ -13,9 +13,18 @@ from os import path
 # -- globals --
 version = 'v1.0'
 
+
+# -- classes --
+class KbspSchedule:
+    def __init__(self):
+        pass
+
+
 # -- functions --
 
 
 # -- launching --
 if __name__ == "__main__":
     ui.greetings(version)
+    x = ui.get_line("Get it?: ")
+    ui.print_line(x)
--- core/ui.py
+++ core/ui.py
@@ -14,13 +14,34 @@ init()
 
 # -- Functions --
 def greetings(version):
+    """Print the intro.
+
+    • version: str - version of program
+    """
     with open(os.path.join('core', 'static', 'logo'), 'r') as fr:
         print('\n')
         for line in fr:
             print(Fore.BLUE + line, end="")
         print('\n')
-    print(Fore.WHITE + Style.DIM + "             by" + Style.RESET_ALL + Style.BRIGHT + Fore.WHITE + " vjki " + Style.RESET_ALL + Style.DIM + Fore.WHITE + "(@ikjvvjki)" + Style.RESET_ALL, end="\n")
+    print(
+        Fore.WHITE + Style.DIM + "             by" + Style.RESET_ALL + Style.BRIGHT + Fore.WHITE + " vjki " + Style.RESET_ALL + Style.DIM + Fore.WHITE + "(@ikjvvjki)" + Style.RESET_ALL,
+        end="\n")
     print(Fore.CYAN + "  ---------------------------------------")
     print(Fore.CYAN + f"  | {version} | ", end="")
     print(Fore.CYAN + Style.DIM + "Monitors the schedule change" + Style.RESET_ALL + Fore.CYAN + " |")
-    print(Fore.CYAN + "  ---------------------------------------")
+    print(Fore.CYAN + "  ---------------------------------------", end="\n\n")
+
+
+def print_line(message, status='info'):
+    """Print text message in console.
+
+    """
+    if status == 'info':
+        print(Style.DIM + Fore.WHITE + '  [' + Fore.BLUE + 'i' + Fore.WHITE + ']: ' + Style.RESET_ALL + message)
+    elif status == 'warning':
+        print(Style.DIM + Fore.WHITE + '  [' + Fore.RED + '!' + Fore.WHITE + ']: ' + Style.RESET_ALL + message)
+
+
+def get_line(message=""):
+    x = input(Style.DIM + Fore.WHITE + '  [' + Fore.GREEN + '>' + Fore.WHITE + '] ' + Style.RESET_ALL + message)
+    return x
