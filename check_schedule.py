0d282f963f946843c5ca86f581abd38ebf63fb50
vjki <1_0_91@bk.ru>
2021-02-21 14:54:54 +0300

    added some changes

--- check_schedule.py
+++ check_schedule.py
@@ -8,21 +8,34 @@
 #
 from core import ui
 from kbsp_schedule import getting, parsing
-from os import path
+from os import path, mkdir
 
 # -- globals --
 version = 'v1.0'
-commands = [
-    'get',
-    'check',
-    'exit'
-]
+commands = {
+    'help': 'Get list of all commands.',
+    'get': 'Get all schedules files. To check int go [program folder]/schedule.',
+    'check': 'Get time last modified and update schedule.',
+    'exit': 'Exit program.'
+}
 
 
 # -- classes --
 class KbspSchedule:
     def __init__(self):
-        pass
+        """Creating environment.
+
+        """
+        try:
+            mkdir('schedule')
+            mkdir('json')
+            for subdir in range(1, 6):
+                mkdir(path.join('.', 'schedule', str(subdir)))
+                mkdir(path.join('.', 'json', str(subdir)))
+        except FileExistsError:
+            pass
+        f = open(path.join('.', 'schedule', 'lmod.csv'), 'w')
+        f.close()
 
 
 # -- functions --
@@ -31,12 +44,13 @@ def com_check():
 
 
 def com_get():
-        getting.get_schedule(path.join(".", "schedule"))
-        getting.check_schedule(path.join(".", "schedule"))
+    getting.get_schedule(path.join(".", "schedule"))
+    getting.check_schedule(path.join(".", "schedule"))
 
 
 # -- launching --
 if __name__ == "__main__":
+    launch = KbspSchedule()
     ui.greetings(version)
     while True:
         command = ui.get_line()
@@ -45,10 +59,14 @@ if __name__ == "__main__":
 
         if command == 'check':
             com_check()
-        
+
         if command == 'exit':
             ui.print_line("Bye!")
             break
-        
+
+        if command == 'help':
+            for k, v in commands.items():
+                print(f'{k} - {v}')
+
         if command not in commands:
-            ui.print_line(f'I don not know {command} command :(', end='\n\n')
+            ui.print_line(f'I don not know {command} command :(Type \'help\' for commands list', end='\n\n')
