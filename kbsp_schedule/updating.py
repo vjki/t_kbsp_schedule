d395b4a135e7f2a5f734d32337bc8226e95edd5f
klimentlamonov <1_0_91@bk.ru>
2021-04-11 15:13:41 +0300

    chache updating.py

--- kbsp_schedule/updating.py
+++ kbsp_schedule/updating.py
@@ -6,16 +6,20 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 #
+import re
 import csv
 import shutil
 import requests
 
 from os import mkdir, path
+from bs4 import BeautifulSoup
 from datetime import datetime
 from openpyxl import load_workbook
 
 # -- globals --
 temp_dir_name = 'temp'
+url = 'https://www.mirea.ru/schedule/'
+
 
 # -- functions --
 def get_file_links(schedule_dir: str) -> tuple:
@@ -27,33 +31,44 @@ def get_file_links(schedule_dir: str) -> tuple:
             yield row[0], row[1] 
 
 
-def get_file_lenght(link: str):
+def get_file_status(link: str):
     responce = requests.head(link, allow_redirects=True)
-    size = responce.headers['Content-Length']
-    return int(size)
+    status = responce.status_code
+    return status
 
 
 def update_status(schedule_dir: str) -> dict:
     d = {}
-    for cource, link in get_file_links(schedule_dir):
+    for course_link in get_file_links(schedule_dir):
+        link = course_link[1]
         file_name = link.split('/')[-1]
-        full_path = path.join(schedule_dir, cource, file_name)
-        if path.getsize(full_path) != get_file_lenght(link):
+        link_status = get_file_status(link)
+        if link_status == 404:
             d.update({file_name: 1})
-        else:
+        elif link_status == 200:
             d.update({file_name: 0})
     return d
 
 
 def update(schedule_dir: str, d: dict):
     try:
-        for cource, link in get_file_links(schedule_dir):
-            file_name = link.split('/')[-1]
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, features="html.parser")
+        links = soup.find_all(href=re.compile(r"КБиСП.*xlsx"))
+        for link in links:
+            file_name = link.get('href').split('/')[-1]
             if d[file_name] == 1:
-                full_path = path.join(schedule_dir, cource, file_name)
-                with open(full_path, 'wb') as f:
-                    urf = requests.get(link)
+                with open(path.join(schedule_dir, file_name.split()[1], file_name), 'wb') as f:
+                    urf = requests.get(link.get('href'))
                     f.write(urf.content)
+
+        # for cource, link in get_file_links(schedule_dir):
+        #     file_name = link.split('/')[-1]
+        #     if d[file_name] == 1:
+        #         full_path = path.join(schedule_dir, cource, file_name)
+        #         with open(full_path, 'wb') as f:
+        #             urf = requests.get(link)
+        #             f.write(urf.content)
         return True
     except:
         return False
